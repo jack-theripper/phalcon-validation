b031e9e9a433585462c93ec1000b1ac5213f98b7
Dmitry <dmitry.arhitector@yandex.ru>
2018-02-13 09:47:33 +0300

    CombinedFieldsValidator was added

---
+++ src/Validation/CombinedFieldsValidator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Arhitector\Validation;
+
+/**
+ * This is a base class for combined fields validators
+ *
+ * @package Arhitector\Validation
+ */
+abstract class CombinedFieldsValidator extends Validator
+{
+	
+}
