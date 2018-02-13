63c42536a2f110fd8d94892597baf8aabcb83c85
Dmitry <dmitry.arhitector@yandex.ru>
2018-02-13 09:31:57 +0300

    Exception was added

---
+++ src/Validation/Exception.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Arhitector\Validation;
+
+/**
+ * Exceptions thrown in Phalcon\Validation\* classes will use this class
+ *
+ * @package Arhitector\Validation
+ */
+class Exception extends \Exception
+{
+	
+}
