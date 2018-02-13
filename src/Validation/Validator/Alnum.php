c6f00d239688ea2ea371960d1c7aa6e82cf4ebd8
Dmitry <dmitry.arhitector@yandex.ru>
2018-02-13 13:48:34 +0300

    Alnum was added

---
+++ src/Validation/Validator/Alnum.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dmitry
+ * Date: 13.02.2018
+ * Time: 13:45
+ */
+
+namespace Arhitector\Validation\Validator;
+
+
+use Arhitector\Validation;
+use Arhitector\Validation\Message;
+use Arhitector\Validation\Validator;
+
+/**
+ * Phalcon\Validation\Validator\Alnum
+ *
+ * Check for alphanumeric character(s)
+ *
+ * <code>
+ * use Phalcon\Validation;
+ * use Phalcon\Validation\Validator\Alnum as AlnumValidator;
+ *
+ * $validator = new Validation();
+ *
+ * $validator->add(
+ *     "username",
+ *     new AlnumValidator(
+ *         [
+ *             "message" => ":field must contain only alphanumeric characters",
+ *         ]
+ *     )
+ * );
+ *
+ * $validator->add(
+ *     [
+ *         "username",
+ *         "name",
+ *     ],
+ *     new AlnumValidator(
+ *         [
+ *             "message" => [
+ *                 "username" => "username must contain only alphanumeric characters",
+ *                 "name"     => "name must contain only alphanumeric characters",
+ *             ],
+ *         ]
+ *     )
+ * );
+ * </code>
+ *
+ * @package Arhitector\Validation\Validator
+ */
+class Alnum extends Validator
+{
+	
+	/**
+	 * Executes the validation
+	 *
+	 * @param Validation $validation
+	 * @param string     $attribute
+	 *
+	 * @return  bool
+	 * @throws Validation\Exception
+	 */
+	public function validate(Validation $validation, $attribute)
+	{
+		$value = $validation->getValue($attribute);
+		
+		if ( ! ctype_alnum($value))
+		{
+			$label = $this->prepareLabel($validation, $attribute);
+			$message = $this->prepareMessage($validation, $attribute, "Alnum");
+			$code = $this->prepareCode($attribute);
+			
+			$replacePairs = [":field" => $label];
+			
+			$validation->appendMessage(new Message(strtr($message, $replacePairs), $attribute, "Alnum", $code));
+			
+			return false;
+		}
+		
+		return true;
+	}
+	
+}
